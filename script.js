4cc5f12b40fb37bcbc8406898d747e102d94914b
Mihir Bibhuty <77892621+MihirBibhuty@users.noreply.github.com>
2021-09-19 17:03:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,96 +1,4 @@
-
-// Map Code (Ploting Lat and Lng values in a Google Map)
-
-// function initMap() {
-
-//     // Map options
-//     var options = {
-//         zoom: 4,
-//         center: { lat: 28.7041, lng: 77.1025 }
-//     }
-
-//     // Making ner map
-//     var map = new google.maps.Map(document.getElementById('map'), options);
-
-
-
-
-//     // Converting SpreadSheet Values into a JSON
-
-//     var input = document.getElementById('input');
-//     input.addEventListener('change', function () {
-//         readXlsxFile(input.files[0]).then(function (data) {
-//             console.log(data);
-
-//             markerFunc(data);   // Calling markerFunc function and giving it data values as parameter
-//         });
-//     });
-
-
-
-
-
-//     // Putting the JSON values into an Array(markers array) via a function
-
-//     function markerFunc(data) {
-
-//         var markers = [];   // Making an empty array
-
-
-//         // Filling values into the Array by looping
-//         for (let i = 1; i < data.length; i++) {
-
-//             markers[i - 1] = {
-//                 coords: { lat: parseFloat(data[i][1]), lng: parseFloat(data[i][0]) },
-//                 content: data[i][2]
-//             };
-//         }
-
-
-
-//         console.log(markers);   // Checking if the JSON/SpreadSheet values are properly put into the 'markers' Array
-
-
-
-//         // Loop through markers
-//         for (var i = 0; i < markers.length; i++) {
-
-//             // Add marker
-//             addMarker(markers[i]);
-//         }
-
-
-//         // Creating addMarker Function
-//         function addMarker(props) {
-
-//             // Add marker
-//             var marker = new google.maps.Marker({
-//                 position: props.coords,
-//                 map: map,
-//             });
-
-//             // Info window
-//             var infoWindow = new google.maps.InfoWindow({
-//                 content: 'See in abobe text box...'
-//             });
-
-//             // Info window event listner
-//             marker.addListener('click', function () {
-//                 infoWindow.open(map, marker);
-
-//                 let info = document.getElementById('infoBox');
-//                 info.innerHTML = props.content;
-//             });
-//         }
-
-//     }
-
-
-// }
-
-
-
-
+let infoObj = [];
 
 function initMap() {
 
@@ -178,6 +86,8 @@ function initMap() {
         // Creating addMarker Function
         function addMarker(props) {
 
+            var line;
+
             // Add marker
             var marker = new google.maps.Marker({
                 position: props.coords,
@@ -186,19 +96,119 @@ function initMap() {
 
             // Info window
             var infoWindow = new google.maps.InfoWindow({
-                content: 'See in abobe text box...'
+                content: 'See in above text box...'
             });
 
-            // Info window event listner
+            // Info window click listner
             marker.addListener('click', function () {
+
+                closeOtherInfo();
+
                 infoWindow.open(map, marker);
+
+                // Draw a line showing the straight distance between the markers
+                // const origin = { lat: 12.8570400, lng: 80.0696210 };
+                // line = new google.maps.Polyline({ path: [origin, props.coords], map: map });
 
                 let info = document.getElementById('infoBox');
                 info.innerHTML = props.content;
-            });
-        }
 
+                infoObj[0] = infoWindow;
+
+
+
+
+                // Driving Distance Calculator -->
+
+                // let directionsService = new google.maps.DirectionsService();
+                // let directionsRenderer = new google.maps.DirectionsRenderer();
+                // directionsRenderer.setMap(map); // Existing map object displays directions
+                // // Create route from existing points used for markers
+                // const route = {
+                //     origin: origin,
+                //     destination: props.coords,
+                //     travelMode: 'DRIVING'
+                // }
+
+                // directionsService.route(route, function (response, status) { // anonymous function to capture directions
+                //     if (status !== 'OK') {
+                //         window.alert('Directions request failed due to ' + status);
+                //         return;
+                //     }
+                //     else {
+                //         // directionsRenderer.setDirections(response); // Add route to the map
+                //         var directionsData = response.routes[0].legs[0]; // Get data about the mapped route
+
+                //         if (!directionsData) {
+                //             window.alert('Directions request failed');
+                //             return;
+                //         }
+                //         else {
+                //             // document.getElementById('msg').innerHTML += " Driving distance is " + directionsData.distance.text + " (" + directionsData.duration.text + ").";
+                //             console.log(directionsData);
+                //         }
+                //     }
+                // });
+
+
+
+
+
+            });
+
+
+
+
+
+
+            function closeOtherInfo() {
+
+                // document.getElementById('infoBox').innerHTML = "";
+
+                if (infoObj.length > 0) {
+                    infoObj[0].set("marker", null);
+                    infoObj[0].close();
+                    infoObj[0].length = 0;
+                }
+            }
+
+            // let place = document.getElementById('map')
+            // place.addEventListener('click', function () {
+            //     console.log(line);
+
+            //     delete line;
+
+            //     console.log(line);
+            //     console.log("clicked on map");
+            // })
+
+        }
     }
+
+
+
+
+
+    // Marking the place fron which distance is to be calculated.
+
+    // Add marker
+    var marker = new google.maps.Marker({
+        position: { lat: 12.8570400, lng: 80.0696210 },
+        map: map,
+//         icon: new google.maps.MarkerImage("http://www.googlemapsmarkers.com/v1/009900/")
+
+    });
+
+    // Info window
+    var infoWindow = new google.maps.InfoWindow({
+        content: '<h6>Naattarasan <br/>(Chettinad Restaurant)</h6>'
+    });
+
+    // Info window event listner
+    marker.addListener('click', function () {
+        infoWindow.open(map, marker);
+    });
+
 
 
 }
@@ -280,7 +290,3 @@ function noOfRows(data) {
     let textNode = document.createTextNode(`Total number of values given --> ${data.length - 1}`);
     document.getElementById('tblData').appendChild(textNode);
 }
-
-
-
-
